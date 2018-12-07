7e0436ded126bcafb313a2c84852911914ba96b8
Yen Yin Hing <yenyinf287@gmail.com>
2018-12-06 19:11:35 -0800

    scoreboard and ping

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -2,6 +2,7 @@ import { SVG_NS, KEYS } from '../settings';
 import Board from './Board';
 import Paddle from './Paddle';
 import Ball from './Ball';
+import Score from './Score';
 
 export default class Game {
 
@@ -9,78 +10,85 @@ export default class Game {
 		this.element = element;
 		this.width = width;
 		this.height = height;
+		this.pause = false;
 
-		this.gameElement = document.getElementById(this.element); // getting this.element = <div> game</div>
-		   
-	
-
-		this.paddleWidth = 8; 
-		this.paddleHeight = 56; 
-		this.boardGap = 10; 
-		this.ballRadius = 8; 
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.boardGap = 10;
+		this.ballRadius = 8;
+		this.scoreFontSize = 36;
 		this.gameElement = document.getElementById(this.element);
 
 		this.board = new Board(this.width, this.height);
 
-		this.player1 = new Paddle (  //constructor call / tell game where we creating it
-			this.height, 
+		this.player1 = new Paddle(  //constructor call / tell game where we creating it
+			this.height,
 			this.paddleWidth,
-			this.paddleHeight, 
-			this.boardGap, 
-			((this.height - this.paddleHeight) /2), 
-			KEYS.p1up, 
+			this.paddleHeight,
+			this.boardGap,
+			((this.height - this.paddleHeight) / 2),
+			KEYS.p1up,
 			KEYS.p1down,
 
 		);
 
-		this.player2 = new Paddle (  //constructor call / tell game where we creating it
-			this.height, 
+		this.player2 = new Paddle(  //constructor call / tell game where we creating it
+			this.height,
 			this.paddleWidth,
-			this.paddleHeight, 
+			this.paddleHeight,
 			(this.width - this.boardGap - this.paddleWidth),
-			((this.height - this.paddleHeight) / 2), 
-		
+			((this.height - this.paddleHeight) / 2),
+
 			KEYS.p2up,
 			KEYS.p2down,
 
 		);
 
 		this.ball = new Ball(this.ballRadius, this.width, this.height);
+		
+		// this.secondBall = new Ball(this.ballRadius, this.width, this.height);
+
+		this.score1 = new Score(this.width /2 - 50 ,30, this.scoreFontSize);
+		this.score2 = new Score(this.width /2 + 25 ,30, this.scoreFontSize);
+
+
+		 document.addEventListener('keydown', event => { 
+		    switch(event.key) {
+			case KEYS.spaceBar:
+			this.pause =!this.pause
+           break;
+            }
+        });
 
 	}
 
 	render() {
-		
+		if(this.pause) { 
+		return; 
+		}
 		//anything that will change - go to render method
-		this.gameElement.innerHTML ='';   
+		this.gameElement.innerHTML = '';
 		let svg = document.createElementNS(SVG_NS, 'svg');
 		svg.setAttributeNS(null, 'width', this.width);
 		svg.setAttributeNS(null, 'height', this.height);
 		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
 		this.gameElement.appendChild(svg);
 
-		this.board.render(svg); 
+		this.board.render(svg);
 
 		this.player1.render(svg);
 		this.player2.render(svg);
 
-		this.ball.render(svg); 
-		// this.score1.render(svg); 
-		// this.score2.render(svg);
+		this.ball.render(svg, this.player1, this.player2);
+		// this.secondBall.render(svg, this.player1, this.player2);
+
+		this.score1.render(svg, this.player1.getScore()); 
+		this.score2.render(svg, this.player2.getScore());
+
+
+		if (this.player1.getScore > this.player2.getScore) { 
+			return "      "
+		}
 	}
 
-} 
-
-// Other code goes here...
-
-// 		const ball = new Ball(startXpos, startYPos);
-//         const paddle1 = new paddle1(controlUp, controlDown, x, y, width, height);
-// 		const paddle2 = new paddle2(controlUp1, controlDown1, x1, y1, width, height);
-// 		const board = new Board(width, height);
-// 		let svg = document.createElementNS(SVG_NS, "svg");
-// svg.setAttributeNS(null, "width", this.width);
-// svg.setAttributeNS(null, "height", this.height);
-// svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
-
-// this.gameElement ='';
-// this.gameElement.appendChild(this.gameElement);
+}
--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -6,7 +6,7 @@ constructor (boardHeight,width, height, x, y, up, down) {
     this.boardHeight = boardHeight; 
     this.width = width; 
     this.height = height; 
-    this.x = x; // passing in the 
+    this.x = x; 
     this.y = y; 
     this.up = up; 
     this.down = down; 
@@ -19,7 +19,7 @@ constructor (boardHeight,width, height, x, y, up, down) {
             this.y = Math.max(0,this.y - this.speed); // max =  0 moves up by 10 px. 
             break; 
             case this.down:
-            this.y = Math.min(this.boardHeight - this.height, this.y + this.speed); 
+            this.y = Math.min(this.boardHeight - this.height, this.y + this.speed);  
             break;
 
         }
@@ -27,6 +27,22 @@ constructor (boardHeight,width, height, x, y, up, down) {
 
 }
 
+increaseScore() { 
+    this.score += 1;
+}
+
+getScore() { 
+    return this.score;
+}
+
+coordinates(){
+const leftX = this.x; 
+const rightX = this.x + this.width;
+const topY = this.y;
+const bottomY = this.y + this.height; 
+return [leftX, rightX, topY, bottomY];
+
+}
 render(svg){ 
 
     let rect = document.createElementNS(SVG_NS, 'rect'); 
